38da39f70e2f33ea05ad81baad3ad704f6b106f3
Jose Llamas <pep84c@gmail.com>
2026-08-20 08:34:52 -0700

    Add NEAREST_NEIGHBOR/BILINEAR/BICUBIC image interpolation constants

--- src/Processing.h
+++ src/Processing.h
@@ -1389,6 +1389,10 @@ static constexpr int ENABLE_ASYNC_SAVEFRAME    = -12;
 static constexpr int NEAREST_NEIGHBOR = 0;
 static constexpr int BILINEAR         = 1;
 static constexpr int BICUBIC          = 2;
+// Image interpolation modes
+static constexpr int NEAREST_NEIGHBOR = 0;
+static constexpr int BILINEAR         = 1;
+static constexpr int BICUBIC          = 2;
 
 // Cursor shapes (map to GLFW)
 static constexpr int ARROW       = GLFW_ARROW_CURSOR;
