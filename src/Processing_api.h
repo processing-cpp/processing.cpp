a710e191e34657972e062f057e056d443081be42
Jose Llamas <pep84c@gmail.com>
2026-08-23 15:43:28 -0700

    Fix Processing_api.h: restore header comment, add deltaTime forwarder

--- src/Processing_api.h
+++ src/Processing_api.h
@@ -1,4 +1,6 @@
-// Processing_api.h
+/
+inline float deltaTime()   { return ::Processing::PApplet::g_papplet ? ::Processing::PApplet::g_papplet->deltaTime : 0.016f; }
+/Processing_api.h
 // Included in Sketch_run.cpp to make Processing API available to user-defined
 // classes inside sketches (which don't inherit from PApplet).
 #pragma once
